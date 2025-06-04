942388e49bffc00693185c5142c62c4526a3827d
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2025-06-04 18:04:51 -0500

    feat: add logout endpoint to clear 'access-token' cookie

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,19 +3,25 @@ import {
 	Post,
 	Body,
 	UseInterceptors,
-	Req,
 	Res,
 	HttpStatus,
 } from "@nestjs/common";
+import { CookieOptions, Response } from "express";
 
 import { AuthService } from "./auth.service";
 import { LoginUserDto } from "./dto/login-user.dto";
 import { NatsMessagesInterceptor } from "src/common/interceptors/natsMessages.interceptor";
-import { Response } from "express";
 
 @UseInterceptors(NatsMessagesInterceptor)
 @Controller("auth")
 export class AuthController {
+	private readonly COOKIE_OPTIONS: CookieOptions = {
+		httpOnly: true,
+		secure: false,
+		sameSite: "lax",
+		maxAge: 1000 * 60 * 60 * 24,
+	};
+
 	constructor(private readonly authService: AuthService) {}
 
 	@Post("/login")
@@ -28,12 +34,11 @@ export class AuthController {
 
 		// 2. If login is successful, add the JWT to the cookie.
 		if (msResponse.code === 200) {
-			response.cookie("access-token", msResponse.message.token, {
-				httpOnly: true,
-				secure: false,
-				sameSite: "lax",
-				maxAge: 1000 * 60 * 60 * 24,
-			});
+			response.cookie(
+				"access-token",
+				msResponse.message.token,
+				this.COOKIE_OPTIONS,
+			);
 
 			const { username, id } = msResponse.message;
 
@@ -45,6 +50,16 @@ export class AuthController {
 		}
 
 		return msResponse;
+	}
+
+	@Post("/logout")
+	logOut(@Res({ passthrough: true }) response: Response) {
+		response.clearCookie("access-token", this.COOKIE_OPTIONS);
+		return {
+			code: HttpStatus.OK,
+			status: HttpStatus[HttpStatus.OK],
+			message: "Logout successful",
+		};
 	}
 
 	@Post("/verify")
