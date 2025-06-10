ddc617446898621807031c3a6f67aa5b35fa88ad
Hector Fabio Romero Bocanegra <hector.f.romero28@gmail.com>
2025-06-10 16:42:06 -0500

    feat: set JWT in the "access-token" cookie when a user is created

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,22 +8,55 @@ import {
 	Delete,
 	ParseUUIDPipe,
 	UseInterceptors,
+	Res,
+	HttpStatus,
 } from "@nestjs/common";
 import { UUID } from "node:crypto";
+import { CookieOptions, Response } from "express";
 
 import { UsersService } from "./users.service";
 import { CreateUserDto } from "./dto/create-user.dto";
 import { UpdateUserDto } from "./dto/update-user.dto";
 import { NatsMessagesInterceptor } from "src/common/interceptors/natsMessages.interceptor";
+import { PublicRoute } from "src/common/decorators/public-route.decorator";
 
 @UseInterceptors(NatsMessagesInterceptor)
 @Controller("users")
 export class UsersController {
+	private readonly COOKIE_OPTIONS: CookieOptions = {
+		httpOnly: true,
+		secure: false,
+		sameSite: "lax",
+		maxAge: 1000 * 60 * 60 * 24,
+	};
+
 	constructor(private readonly usersService: UsersService) {}
 
+	@PublicRoute()
 	@Post()
-	create(@Body() createUserDto: CreateUserDto) {
-		return this.usersService.create(createUserDto);
+	async create(
+		@Res({ passthrough: true }) response: Response,
+		@Body() createUserDto: CreateUserDto,
+	) {
+		const newUser = await this.usersService.create(createUserDto);
+
+		console.log("EN CONTROLLER");
+		console.log(newUser);
+		if (newUser.code === 200) {
+			response.cookie(
+				"access-token",
+				newUser.message.token,
+				this.COOKIE_OPTIONS,
+			);
+
+			return {
+				code: HttpStatus.OK,
+				status: HttpStatus[HttpStatus.OK],
+				message: newUser.message.user,
+			};
+		}
+
+		return newUser;
 	}
 
 	@Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
-import { Inject, Injectable } from "@nestjs/common";
+import {
+	BadRequestException,
+	HttpStatus,
+	Inject,
+	Injectable,
+} from "@nestjs/common";
 import { ClientProxy } from "@nestjs/microservices";
 import { firstValueFrom } from "rxjs";
 import { UUID } from "node:crypto";
@@ -15,10 +20,38 @@ export class UsersService {
 	) {}
 
 	async create(createUserDto: CreateUserDto) {
-		const response = await firstValueFrom(
+		// 1. Create the user in DB
+		const userMSresponse = await firstValueFrom(
 			this.client.send("users.create", createUserDto),
 		);
-		return response;
+
+		if (userMSresponse.code !== 200) {
+			return userMSresponse;
+		}
+
+		console.log(userMSresponse);
+		const { id, username } = userMSresponse.message;
+
+		// 2. Generate JWT for the new user.
+		const authMSResponse = await firstValueFrom(
+			this.client.send("auth.signup", { id, username }),
+		);
+
+		if (authMSResponse.code !== 200) {
+			return authMSResponse;
+		}
+
+		console.log("AUTH RESPONSE");
+		console.log(authMSResponse);
+
+		return {
+			code: HttpStatus.OK,
+			status: HttpStatus[HttpStatus.OK],
+			message: {
+				user: userMSresponse.message,
+				token: authMSResponse.message.token,
+			},
+		};
 	}
 
 	async findAll() {
